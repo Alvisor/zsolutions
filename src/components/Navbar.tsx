ebc9c95907050c47d36c7b57a31e2651022cf824
Manuel Alejandro Rodriguez Rojas <alvisor.max@gmail.com>
2025-02-07 20:27:30 -0500

    UPD: Se ajusta el logo en el nav bar ADD: Se agrega el insumo del logo en la carpeta correspondiente

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
-import "../styles/global.css"; 
+import "../styles/global.css";
+import logo from "../assets/logos/ZSolutionsLogo.png"; 
 
 const Navbar = () => {
   return (
-    <nav>
-      <h1>Z Solutions</h1>
-      <div>
+    <nav className="navbar">
+      {/* Logo con enlace a inicio */}
+      <Link to="/" className="logo-container">
+        <img src={logo} alt="Z Solutions Logo" className="logo" />
+      </Link>
+
+      {/* Links de navegación */}
+      <div className="nav-links">
         <Link to="/">Inicio</Link>
         <Link to="/about">¿Quiénes Somos?</Link>
         <Link to="/services">Servicios</Link>
