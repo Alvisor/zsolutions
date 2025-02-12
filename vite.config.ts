35dc66654f0fa86afaddc1e7edee13dde06dbdb8
Manuel Alejandro Rodriguez Rojas <alvisor.max@gmail.com>
2025-02-11 21:58:42 -0500

    UPD: nUEVO AJUSTE EN VITE PARA INDICAR LAS RUTAS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; 
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: './', // Hace que las rutas sean relativas
+  build: {
+    outDir: "dist", // Asegura que el output vaya a la carpeta correcta
+  },
 });
